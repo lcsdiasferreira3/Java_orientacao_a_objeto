7537df22cb18e44373a791d4b0a6058847060007
lcsdiasferreira3 <lcsdferreira3@gmail.com>
2019-06-09 04:12:26 -0300

    orientacao_a_obj

---
+++ src/Aula10/Aula10.java
@@ -0,0 +1,20 @@
+package Aula10;
+
+public class Aula10 {
+
+	
+	public static void main(String[] args) {
+		Conta minhaConta;
+		minhaConta = new Conta();
+		
+		minhaConta.titular ="Duke";
+		minhaConta.saldo = 1000;
+		
+		minhaConta.saca(200);
+		
+		minhaConta.deposita(500);
+		System.out.println(minhaConta.saldo);
+		
+	}
+	
+}
